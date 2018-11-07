328b6716a6fb6318057c535f6b41e8b62e197dcf
Deron Nicholson <nicholsonderon95@gmail.com>
2018-11-07 00:42:47 -0500

    Stage 3

--- fifteen.js
+++ fifteen.js
@@ -60,4 +60,73 @@ window.onload = function(){
 	});
 
 
+	//sets up board
+	for(var i=0; i < pieces.length; i++){
+		pieces[i].className = "puzzlepiece";
+		pieces[i].style.top =  positionTop + "px";
+		pieces[i].style.left = leftposition + "px";
+		pieces[i].style.backgroundPosition = backgroundLeft + "px " + backgroundTop + "px";
+		pieces[i].onclick= move;
+		pieces[i].onmouseover= movable;
+
+		if(leftposition < 300){
+			leftposition = leftposition + 100;
+			backgroundLeft = backgroundLeft - 100;
+		}
+		else{
+			leftposition = 0;
+			backgroundLeft = 0;
+			positionTop = positionTop + 100;
+			backgroundTop = backgroundTop - 100;
+		}	
+	}
+
 	
+	//checks if piece is povable and if so it adds movable style to it and if not removes movable style from it
+	function movable(){
+		oldTop = parseInt(this.style.top);
+		oldLeft = parseInt(this.style.left);
+		if (oldTop == emptyTop && oldLeft == (eLeft-100) || oldTop == emptyTop && oldLeft == (eLeft+100) || oldTop == (emptyTop-100) && oldLeft == eLeft || oldTop == (emptyTop+100) && oldLeft == eLeft){
+			$(this).addClass('movablepiece');	
+		}
+		else{
+			$(this).removeClass("movablepiece");
+		}
+	}
+
+	//moves puzzle piece
+	function move(){
+		oldTop = parseInt(this.style.top);
+		oldLeft = parseInt(this.style.left);
+		if (oldTop == emptyTop && oldLeft == (eLeft-100) || oldTop == emptyTop && oldLeft == (eLeft+100) || oldTop == (emptyTop-100) && oldLeft == eLeft || oldTop == (emptyTop+100) && oldLeft == eLeft){
+			this.style.top = emptyTop + "px";
+			this.style.left = eLeft + "px";
+			emptyTop = oldTop;
+			eLeft = oldLeft;
+		}
+	}
+
+	
+	function getStyle(top, left){
+		for(var i =0; i < pieces.length; i++){
+			if(pieces[i].style.top==top && pieces[i].style.left==left){
+				shufflePiece = pieces[i];
+				return shufflePiece;		
+			}
+		}
+	}
+
+	//shuffles puzzle pieces
+	function Shuffle(){
+		for(var c = 0; c < shuffleTimes; c++){
+			var choice = Math.floor (Math.random () * 4);
+			console.log(choice);
+			if ( choice == 0) {
+				(getStyle((emptyTop-100)+"px", eLeft+"px"))|| getStyle((emptyTop+100)+"px", eLeft+"px");
+				oldTop = parseInt(shufflePiece.style.top);
+ 				oldLeft = parseInt(shufflePiece.style.left);
+ 				shufflePiece.style.top = emptyTop + "px";
+ 				shufflePiece.style.left = eLeft + "px";
+				emptyTop = oldTop;
+ 				eLeft = oldLeft;
+ 			
